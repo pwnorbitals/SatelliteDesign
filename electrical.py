40e6d205c2f46ec66c4a35310b32ebd6bcc4de75
pwnOrbitals <c.de-claverie@pm.me>
2020-12-11 10:54:21 +0100

    fix electrical

--- electrical.py
+++ electrical.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-T = 86142/(60*60)              # orbital period, seconds to change
+T = 86142/(60*60)              # orbital period, hours
 Psl = 1e3 
 Pslt = 2e3
 Tt = 24
 nreg = 0.93
 nbat = 0.91
-Tt_avg = 0.05* T    # Eclipse time per period, seconds
+Tt_avg = 0.05* T    # Eclipse time per period, hours
 
-Te = Tt_avg # Eclipse time, seconds
-Td = T - Te # Day time, seconds
+Te = Tt_avg # Eclipse time, hours
+Td = T - Te # Day time, hours
 Ka = 1.2    # GEO form factor
 
 Pgs = (Psl/nreg) * (1 + ((Te/Td)/nbat))
@@ -25,6 +25,6 @@ DOD2 = 0.7
 Wbat1 = (Pslt*Tt) / (DOD1 - DOD_init)
 Wbat2 = (Psl*Te) / DOD2
 Wbat = max([Wbat1, Wbat2])
-Mbat = 10 * Wbat # 10 kg / kWh
+Mbat = Wbat / 100                  # 10 kg / kWh
 
 print("Wbat1, Wbat2, Wbat, Mbat :", [Wbat1, Wbat2, Wbat, Mbat])
